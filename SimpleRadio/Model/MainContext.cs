0d447443a20838486a32fef383a274ad9c662a4a
j-rab <petehuber@gmx.net>
2018-03-04 19:14:55 +0100

    More preparations for recordings to schedule

--- SimpleRadio/Model/MainContext.cs
+++ SimpleRadio/Model/MainContext.cs
@@ -47,21 +47,46 @@ namespace SimpleRadio.Model
             set { _favoriteStations = value; }
         }
 
+        private ObservableCollection<Recorder> _recorderList;
+
+        public ObservableCollection<Recorder> recorderList
+        {
+            get { return _recorderList; }
+            set { _recorderList = value;
+                OnPropertyChanged("recorderList");
+            }
+        }
+
+
 
         public ICommand commandGetAllStations { get; set; }
         public ICommand commandSearchStations { get; set; }
+        public ICommand commandAddRecorder { get; set; }
+        public ICommand commandRemoveRecorder { get; set; }
 
         public MainContext()
         {
             this._communicator = new DirbleCommunicator();
-            this.commandGetAllStations = new RelayCommand(param => this.getAllStations());
-            this.commandSearchStations = new RelayCommand(param => this.searchStations());
+            this.commandGetAllStations = new RelayCommand(param => getAllStations());
+            this.commandSearchStations = new RelayCommand(param => searchStations());
+            this.commandAddRecorder = new RelayCommand(param => addRecorder());
             this.favoriteStations = new ObservableCollection<Station>(loadFavoriteStations());
+            this.recorderList = new ObservableCollection<Recorder>{ new Recorder() };
         }
 
         public void addFavoriteStation(Station stationToAdd)
         {
             this.favoriteStations.Add(stationToAdd);
+        }
+
+        private void addRecorder()
+        {
+            this.recorderList.Add(new Recorder());
+        }
+
+        private void removeRecorder(Recorder recorderToRemove)
+        {
+
         }
 
         private List<Station> loadFavoriteStations()
---
+++ SimpleRadio/Model/Recorder.cs
@@ -0,0 +1,54 @@
+﻿using SimpleRadio.Helper;
+using System;
+
+namespace SimpleRadio.Model
+{
+    class Recorder : BaseModel
+    {
+        private bool _isActivated;
+
+        public bool isActivated
+        {
+            get { return _isActivated; }
+            set { _isActivated = value;
+                OnPropertyChanged("isActivated");
+            }
+        }
+
+
+        private DateTime _startTime;
+
+        public DateTime startTime
+        {
+            get { return _startTime; }
+            set { _startTime = value;
+                OnPropertyChanged("startTime");
+            }
+        }
+
+        private DateTime _endTime;
+
+        public DateTime endTime
+        {
+            get { return _endTime; }
+            set { _endTime = value;
+                OnPropertyChanged("endTime");
+            }
+        }
+
+        private Station _selectedStation;
+
+        public Station selectedStation
+        {
+            get { return _selectedStation; }
+            set { _selectedStation = value;
+                OnPropertyChanged("selectedStation");
+            }
+        }
+
+        public Recorder()
+        {
+
+        }
+    }
+}
